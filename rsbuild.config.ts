04bf8174c10fc563f2bf5bcf6593b3a2f4565a48
liaoyali <liaoyali@bytedance.com>
2025-07-25 10:23:03 +0800

    chore: docs

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,21 +2,23 @@ import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
+  output: {
+    distPath: {
+      root: "docs",
+    },
+    assetPrefix: "./",
+  },
   plugins: [pluginReact()],
   source: {
     entry: {
-      index: './src/entry.tsx',
+      index: "./src/entry.tsx",
     },
   },
   tools: {
     postcss: {
       postcssOptions: {
-        plugins: [
-          require('tailwindcss'),
-          require('autoprefixer'),
-        ],
+        plugins: [require("tailwindcss"), require("autoprefixer")],
       },
     },
   },
 });
-      
